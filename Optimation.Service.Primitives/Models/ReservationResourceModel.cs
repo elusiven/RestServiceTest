63347c1ba16757252fd81bae14db808bc0ea26ac
Peter Kujawski <napstegucio124@gmail.com>
2020-01-21 02:15:31 +0100

    Added resource models for expense and reservation

---
+++ Optimation.Service.Primitives/Models/ReservationResourceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimation.Service.Primitives.Models
+{
+    public class ReservationResourceModel
+    {
+        public string Vendor { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
