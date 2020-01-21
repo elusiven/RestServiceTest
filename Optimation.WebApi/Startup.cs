7092a5c37cb2e5f7e8da10c87f834db8d39bb73f
Peter Kujawski <napstegucio124@gmail.com>
2020-01-21 01:20:56 +0100

    Added swagger documentation to the web API project

--- Optimation.WebApi/Startup.cs
+++ Optimation.WebApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Optimation.Service.ModuleRegistration;
 using System;
 
@@ -23,6 +24,17 @@ namespace Optimation.WebApi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Register swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Optimation REST API", 
+                    Version = "v1", 
+                    Description = "Documentation of Optimation REST API" 
+                });
+            });
+
             return new AutofacServiceProvider(BuildAutofacContainer(services));
         }
 
@@ -37,10 +49,26 @@ namespace Optimation.WebApi
                 app.UseHsts();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Optimation Rest API V1");
+                // Serve swagger at the route
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
 
+        /// <summary>
+        /// Builds a DI container from all registered components
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         private IContainer BuildAutofacContainer(IServiceCollection services)
         {
             ContainerBuilder builder = new ContainerBuilder();
@@ -49,6 +77,10 @@ namespace Optimation.WebApi
             return builder.Build();
         }
 
+        /// <summary>
+        /// Register other required dependencies from other assemblies
+        /// </summary>
+        /// <param name="builder"></param>
         private void RegisterAdditionalDependencies(ContainerBuilder builder)
         {
             builder.RegisterAssemblyModules(typeof(ServiceModule).Assembly);
